4f901ce2f2dab8944c14d3feb5c75863658c8538
AceeeDev <cyberandrew03@gmail.com>
2025-10-25 14:00:45 -0700

    Added phases to next turn

--- The Forge/Assets/Scripts/Managers/GameManager.cs
+++ The Forge/Assets/Scripts/Managers/GameManager.cs
@@ -68,6 +68,10 @@ public class GameManager : MonoBehaviour
 
     public void NextTurn()
     {
+        /// will switch the phase and scene and round++ if True
+        bool moveToNextPhase = false;
+
+        // switch current turn
         if (currentTurn == CurrentTurn.Player1)
         {
             currentTurn = CurrentTurn.Player2;
@@ -75,6 +79,46 @@ public class GameManager : MonoBehaviour
         else
         {
             currentTurn = CurrentTurn.Player1;
+        }
+
+        // if draft phase
+        if (currentPhase == CurrentPhase.Draft)
+        {
+            if (DeckManager.inst.lastTurn)
+            {
+                moveToNextPhase = true;
+            
+                DeckManager.inst.lastTurn = false;
+            }
+
+            // check if all decks are full and ready to go to the next phase
+            else if (DeckManager.inst.player1Deck.IsDeckFull() && DeckManager.inst.player2Deck.IsDeckFull())
+            {
+                moveToNextPhase = true;
+            }
+        }
+        // if action phase
+        else
+        {
+
+        }
+
+        if (moveToNextPhase)
+        {
+            if (currentPhase == CurrentPhase.Draft)
+            {
+                currentPhase = CurrentPhase.Action;
+                currentRound += 1;
+
+                SceneManager.LoadScene("ActionScene");
+            }
+            else
+            {
+                currentPhase = CurrentPhase.Draft;
+                currentRound += 1;
+
+                SceneManager.LoadScene("DraftScene");
+            }
         }
     }
 }
