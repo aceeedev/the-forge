d2e0103f1a3065c6f60404c2359120521c21b79b
Riley Wong <yelirgnow@gmail.com>
2025-10-25 16:48:19 -0700

    Add hover rotate

---
+++ The Forge/Assets/Scripts/HoverRotate.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoverRotate : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private float transitionSpeed = 10f;
+    [SerializeField] private float hoverRotation = 10f; // Rotation in degrees
+    
+    private Quaternion originalRotation;
+    private Quaternion targetRotation;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        originalRotation = transform.localRotation;
+        targetRotation = originalRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Smoothly interpolate between current rotation and target rotation
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, Time.deltaTime * transitionSpeed);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if ( GameManager.inst != null && GameManager.inst.currentTurn == GameManager.CurrentTurn.Player1 )
+            targetRotation = originalRotation * Quaternion.Euler(0, 0, hoverRotation);
+        else
+            targetRotation = originalRotation * Quaternion.Euler(0, 0, -hoverRotation);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        targetRotation = originalRotation;
+    }
+}
