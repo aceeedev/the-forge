4335925893e8911b9d4cf5f6c41cb7ef829d54e9
Riley Wong <yelirgnow@gmail.com>
2025-10-25 18:57:08 -0700

    Create card select tweening

--- The Forge/Assets/Scripts/Fetches/CardFetch.cs
+++ The Forge/Assets/Scripts/Fetches/CardFetch.cs
@@ -4,6 +4,7 @@ using UnityEngine.EventSystems;
 using System.Runtime.InteropServices;
 using System;
 using UnityEngine.UI;
+using System.Collections;
 
 public class CardFetch : MonoBehaviour, IPointerDownHandler
 {
@@ -17,6 +18,7 @@ public class CardFetch : MonoBehaviour, IPointerDownHandler
 
     private TextMeshProUGUI cardText;
     private Image image;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -73,45 +75,114 @@ public class CardFetch : MonoBehaviour, IPointerDownHandler
         }
     }
 
+    private System.Collections.IEnumerator TweenCardToTarget(GameObject targetObject)
+    {
+        RectTransform rectTransform = GetComponent<RectTransform>();
+        RectTransform targetRectTransform = targetObject.GetComponent<RectTransform>();
+        
+        Vector3 startPosition = rectTransform.position;
+        Vector3 targetWorldPosition = targetRectTransform.position;
+        
+        float duration = 0.5f;
+        float elapsed = 0f;
+        
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = elapsed / duration;
+            // Use ease-in-out for smooth motion
+            float easedT = t * t * (3f - 2f * t);
+            rectTransform.position = Vector3.Lerp(startPosition, targetWorldPosition, easedT);
+            yield return null;
+        }
+        
+        // Ensure final position is exact
+        rectTransform.position = targetWorldPosition;
+        
+        // After animation, disable the card
+        setActiveChildren(false);
+        image.enabled = false;
+    }
+
+    private System.Collections.IEnumerator HandleCardSelection(int cardIndex, int playerDeckIndex, int otherPoolCardIndex, 
+        CardFetch targetPlayer1Card, CardFetch targetPlayer2Card, CardFetch otherPoolCard)
+    {
+        // Start both tween animations
+        Coroutine tween1 = null;
+        Coroutine tween2 = null;
+
+        if (GameManager.inst.currentTurn == GameManager.CurrentTurn.Player1 && targetPlayer1Card != null)
+        {
+            tween1 = StartCoroutine(TweenCardToTarget(targetPlayer1Card.gameObject));
+        }
+        else if (GameManager.inst.currentTurn == GameManager.CurrentTurn.Player2 && targetPlayer2Card != null)
+        {
+            tween1 = StartCoroutine(TweenCardToTarget(targetPlayer2Card.gameObject));
+        }
+
+        if (otherPoolCard != null)
+        {
+            if (GameManager.inst.currentTurn == GameManager.CurrentTurn.Player1 && targetPlayer2Card != null)
+            {
+                tween2 = otherPoolCard.StartCoroutine(otherPoolCard.TweenCardToTarget(targetPlayer2Card.gameObject));
+            }
+            else if (GameManager.inst.currentTurn == GameManager.CurrentTurn.Player2 && targetPlayer1Card != null)
+            {
+                tween2 = otherPoolCard.StartCoroutine(otherPoolCard.TweenCardToTarget(targetPlayer1Card.gameObject));
+            }
+        }
+
+        // Wait for animations to complete (0.5 seconds default duration)
+        yield return new WaitForSeconds(0.5f);
+
+        // After animations complete, update the deck data
+        if (GameManager.inst.currentTurn == GameManager.CurrentTurn.Player1)
+        {
+            DeckManager.inst.player1Deck.cards[playerDeckIndex] = DeckManager.inst.poolCards[cardIndex];
+            DeckManager.inst.player2Deck.cards[playerDeckIndex] = DeckManager.inst.poolCards[otherPoolCardIndex];
+        }
+        else
+        {
+            DeckManager.inst.player2Deck.cards[playerDeckIndex] = DeckManager.inst.poolCards[cardIndex];
+            DeckManager.inst.player1Deck.cards[playerDeckIndex] = DeckManager.inst.poolCards[otherPoolCardIndex];
+        }
+
+        yield return new WaitForSeconds(0.1f);
+        GameManager.inst.NextTurn();
+    }
+
     public void OnPointerDown(PointerEventData eventData)
     {
         if (cardType == CardType.Pool)
         {
-            // send message to add card to the proper players deck and to remove this card from the pool
-
             int playerDeckIndex = (int)Math.Floor((double)cardIndex / 2);
             int otherPoolCardIndex = cardIndex % 2 == 0 ? cardIndex + 1 : cardIndex - 1;
 
-            if (GameManager.inst.currentTurn == GameManager.CurrentTurn.Player1)
-            {
-                DeckManager.inst.player1Deck.cards[playerDeckIndex] = DeckManager.inst.poolCards[cardIndex];
-
-                DeckManager.inst.player2Deck.cards[playerDeckIndex] = DeckManager.inst.poolCards[otherPoolCardIndex];
-            }
-            else
-            {
-                DeckManager.inst.player2Deck.cards[playerDeckIndex] = DeckManager.inst.poolCards[cardIndex];
-
-                DeckManager.inst.player1Deck.cards[playerDeckIndex] = DeckManager.inst.poolCards[otherPoolCardIndex];
-            }
-
-            // remove the cards visually from the pool
-            setActiveChildren(false);
-            image.enabled = false;
-
-            // Find and disable the other pool card
+            // Find the specific target cards in the player decks at the calculated index
             CardFetch[] allCards = FindObjectsOfType<CardFetch>();
+            CardFetch targetPlayer1Card = null;
+            CardFetch targetPlayer2Card = null;
+            CardFetch otherPoolCard = null;
+
             foreach (CardFetch card in allCards)
             {
-                if (card.cardType == CardType.Pool && card.cardIndex == otherPoolCardIndex)
+                if (card.cardType == CardType.Player1 && card.cardIndex == playerDeckIndex)
                 {
-                    card.setActiveChildren(false);
-                    card.image.enabled = false;
+                    targetPlayer1Card = card;
+                }
+                else if (card.cardType == CardType.Player2 && card.cardIndex == playerDeckIndex)
+                {
+                    targetPlayer2Card = card;
+                }
+                else if (card.cardType == CardType.Pool && card.cardIndex == otherPoolCardIndex)
+                {
+                    otherPoolCard = card;
                 }
             }
 
-            // TODO: maybe switch who goes next?
-            GameManager.inst.NextTurn();
+            // Start the coordinated animation and deck update sequence
+            StartCoroutine(HandleCardSelection(cardIndex, playerDeckIndex, otherPoolCardIndex, 
+                targetPlayer1Card, targetPlayer2Card, otherPoolCard));
         }
     }
 }
